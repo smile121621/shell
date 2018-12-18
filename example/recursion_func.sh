5d8714f1d9d9c6cb443280b7d4e88efc67b90228
wei.lu <wei.lu@mfashion.com.cn>
2018-12-18 10:36:55 +0800

    递归实例

---
+++ example/recursion_func.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# recursion_func.sh
+
+function recursion {
+    if [ $1 -eq 1 ]
+    then
+        echo 1
+    else
+        local temp=$[ $1 - 1 ]
+        local result=$(recursion $temp)
+        echo $[ $result * $1]
+    fi
+}
+
+
+read -p "echo a num: " num
+result=$(recursion $num)
+echo $result
